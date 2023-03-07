435b022f83c9a2fe719ba4272fb381676d05a0b3
marque marque <christopher.ristic@yahoo.fr>
2023-03-07 20:52:02 +0100

    Tourelle audio + mouvement smooth
    Tourelle audio + mouvement smooth

--- Assets/FREE GatlingGun/Turrets/Gatling Gun/Scripts/GatlingGun.cs
+++ Assets/FREE GatlingGun/Turrets/Gatling Gun/Scripts/GatlingGun.cs
@@ -12,7 +12,9 @@ public class GatlingGun : MonoBehaviour
     public Transform go_baseRotation;
     public Transform go_GunBody;
     public Transform go_barrel;
-
+    public AudioClip startShootAudio, shootingAudio, endShootAudio, rotationAudio;
+    public AudioSource src;
+    public AudioSource src2;
     // Gun barrel rotation
     public float barrelRotationSpeed;
     float currentRotationSpeed;
@@ -25,8 +27,13 @@ public class GatlingGun : MonoBehaviour
 
     // Used to start and stop the turret firing
     bool canFire = false;
+    bool hasPlayedStart = false;
 
-    
+    public Transform gunHeadTransform;
+    public float gunHeadRestingHeight = 0.1f;
+    public float restingLerpSpeed = 10.0f;
+
+
     void Start()
     {
         // Set the firing range distance
@@ -35,6 +42,7 @@ public class GatlingGun : MonoBehaviour
 
     void Update()
     {
+
         AimAndFire();
     }
 
@@ -57,14 +65,11 @@ public class GatlingGun : MonoBehaviour
     }
     void OnCollisionStay(Collision other)
     {
-        Debug.Log("find");
         if (other.gameObject.tag == "Enemy")
         {
-            
             go_target = other.transform;
             canFire = true;
         }
-
     }
 
     // Stop firing
@@ -84,32 +89,58 @@ public class GatlingGun : MonoBehaviour
         // if can fire turret activates
         if (canFire)
         {
-            // start rotation
-            currentRotationSpeed = barrelRotationSpeed;
-
             // aim at enemy
-            Vector3 baseTargetPostition = new Vector3(go_target.position.x, this.transform.position.y, go_target.position.z);
-            Vector3 gunBodyTargetPostition = new Vector3(go_target.position.x, go_target.position.y, go_target.position.z);
+            Vector3 baseTargetPosition = new Vector3(go_target.position.x, transform.position.y, go_target.position.z);
+            Vector3 gunBodyTargetPosition = new Vector3(go_target.position.x, go_target.position.y, go_target.position.z);
 
-            go_baseRotation.transform.LookAt(baseTargetPostition);
-            go_GunBody.transform.LookAt(gunBodyTargetPostition);
+            if (!src2.isPlaying)
+                src2.PlayOneShot(rotationAudio);
+
+            Quaternion baseTargetRotation = Quaternion.LookRotation(baseTargetPosition - transform.position);
+            Quaternion gunBodyTargetRotation = Quaternion.LookRotation(gunBodyTargetPosition - go_GunBody.transform.position);
+
+            float baseRotationSpeed = 5f; // Change this value to adjust rotation speed
+            float gunBodyRotationSpeed = 10f; // Change this value to adjust rotation speed
+
+            go_baseRotation.transform.rotation = Quaternion.Slerp(go_baseRotation.transform.rotation, baseTargetRotation, Time.deltaTime * baseRotationSpeed);
+            go_GunBody.transform.rotation = Quaternion.Slerp(go_GunBody.transform.rotation, gunBodyTargetRotation, Time.deltaTime * gunBodyRotationSpeed);
+
 
             // start particle system 
             if (!muzzelFlash.isPlaying)
             {
-                muzzelFlash.Play();
+                if (!src.isPlaying && !hasPlayedStart)
+                {                 
+                    src.PlayOneShot(startShootAudio);
+                    hasPlayedStart = true;
+                }
+                if (src.isPlaying)
+                    // start rotation
+                    currentRotationSpeed = Mathf.Lerp(currentRotationSpeed, barrelRotationSpeed, Time.deltaTime * 0.2f);
+
+                if (!src.isPlaying && hasPlayedStart)
+                {                    
+                    muzzelFlash.Play();
+                    src.clip = shootingAudio;
+                    src.Play();
+                }
             }
         }
+
         else
         {
             // slow down barrel rotation and stop
-            currentRotationSpeed = Mathf.Lerp(currentRotationSpeed, 0, 10 * Time.deltaTime);
+            currentRotationSpeed = Mathf.Lerp(currentRotationSpeed, 0, 2f * Time.deltaTime);
 
             // stop the particle system
-            if (muzzelFlash.isPlaying)
+            if (muzzelFlash.isPlaying || !src.isPlaying && hasPlayedStart)
             {
                 muzzelFlash.Stop();
+                src.Stop();
+                src.PlayOneShot(endShootAudio);
+                hasPlayedStart = false;
             }
+
         }
     }
 }
